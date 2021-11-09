50a6636580b6bde36ce5047867120470ffb4735c
LEEJESEOK <29701999+LEEJESEOK@users.noreply.github.com>
2021-11-09 11:10:22 +0900

    add ObjectRotate.cs

--- Assets/Scripts/ObjectRotate.cs
+++ Assets/Scripts/ObjectRotate.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class ObjectRotate : MonoBehaviour
 {
+    [SerializeField]
+    float roateSpeed = 150;
+    [SerializeField]
+    bool canRotateHorizontal, canRotateVertical;
+    [SerializeField]
+    [Range(0, 60)]
+    float verticalRange;
+
+    float mx, my;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mx = transform.localEulerAngles.x;
+        my = transform.localEulerAngles.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        print(Input.mousePosition);
+        float h = Input.GetAxis("Mouse X");
+        float v = Input.GetAxis("Mouse Y");
+
+        if (canRotateHorizontal)
+        {
+            my += h * roateSpeed * Time.deltaTime;
+        }
+        if (canRotateVertical)
+        {
+            mx += v * roateSpeed * Time.deltaTime;
+        }
+        mx = Mathf.Clamp(mx, -verticalRange, verticalRange);
+
+        transform.localEulerAngles = new Vector3(-mx, my, 0);
     }
 }
